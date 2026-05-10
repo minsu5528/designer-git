81c03e8cf5a17dd1c5eea0ba92ed1db091cc02e1
yeon47 <cerry0724@naver.com>
2026-05-10 15:05:31 +0900

    fix: checkout  시 start_file 대신 base_file을 복사하던 버그 수정

--- src/vcs/vcs.cpp
+++ src/vcs/vcs.cpp
@@ -624,7 +624,7 @@ int checkout(const std::string &repo_path, const std::string &commit_id)
             else
                 start_file = base_file;
 
-            fs::copy_file(base_file, tmp_a,
+            fs::copy_file(start_file, tmp_a,
                           fs::copy_options::overwrite_existing);
 
             // chain[0]은 base 커밋(delta 없음), chain[1]부터 delta 적용
